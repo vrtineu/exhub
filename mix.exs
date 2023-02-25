942f453bba10aeeafba03cc1f30f103c4d9aa631
vrtineu <vininro@gmail.com>
2023-02-25 00:04:02 -0300

    chore: adding tesla and hackney libs

--- mix.exs
+++ mix.exs
@@ -49,7 +49,9 @@ defmodule Exhub.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.15.3", only: :test, runtime: false}
+      {:excoveralls, "~> 0.15.3", only: :test, runtime: false},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
